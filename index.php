105d165c2289bfc8e1e313a222eeb5ac965467c4
pizzacoca <florian.lassenay@aquilenet.fr>
2022-05-23 23:51:09 +0200

    index

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 i<!DOCTYPE html>
 <html>
 <body>
+<H1>BAIOREPAR</H1>
 <button onclick="window.location.href='http://www.baiorepar.pizzacoca.fr/demande.php'">Demander une réparation</button>
-
+<BR>
+<button onclick="window.location.href='demande.php'">Demander une réparation</button>
+<BR>
 <button onclick="window.location.href='http://www.baiorepar.pizzacoca.fr/liste.php'">Voir les demandes</button>
 
 </html> 
