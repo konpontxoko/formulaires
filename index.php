6e514d18e2a4d36e016e20d74e34f02bb3615b98
pizzacoca <florian.lassenay@aquilenet.fr>
2022-05-28 17:20:40 +0200

    mise en page index.php

--- index.php
+++ index.php
@@ -10,48 +10,11 @@
   </style>
   <link href="./css/article-list.css" rel="stylesheet">
   <link href="./css/admin.css" rel="stylesheet">
+  <script type="text/javascript">
+  </script>
+ 
 </head>
- <style>
-    .Admin {
-      background: #ddd;
-      height: 100%;
-      display: grid;
-      grid-template-rows: 1fr 1fr 1fr;
-      grid-template-columns: 1fr 1fr 1fr;
-      grid-template-areas: 
-      ". . ."
-      "new pending archives"
-      ". . database";
-    }
-    .Admin > * {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      text-decoration: none;
-      color: black;
-      text-transform: uppercase;
-    }
-    .Admin > *:hover {
-      font-weight: bold;
-    }
-    .Admin-new {
-      grid-area: new;
-      background: #eee;
-    }
-    .Admin-pending {
-      grid-area: pending;
-      background: #ccc;
-    }
-    .Admin-archives {
-      grid-area: archives;
-      background: #eee;
-    }
-    .Admin-database {
-      grid-area: database;
-      background: #ccc;
-    }
-  </style>
-  
+ 
   <body class="Admin">
     <a class="Admin-new" href="./baiorepar.php">BAIOREPAR</a>
     <a class="Admin-pending" href="./boutik.php">BOUTIK</a>
