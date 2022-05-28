9a46583a2be67f51739dc12ee50ea6dc1e89b9b3
pizzacoca <florian.lassenay@aquilenet.fr>
2022-05-28 09:18:48 +0200

    #1 test

---
+++ boutik/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="./article-list-item.js"></script>
+  <script src="../ux/menu.js"></script>
+  <style type="text/css">
+    body {
+      padding: 50px 10px;
+    }
+  </style>
+  <link href="./article-list.css" rel="stylesheet">
+</head>
+
+
+<body>
+<H1>BAIOREPAR</H1>
+<BR>
+<button onclick="window.location.href='demande.php'">Demander une réparation</button>
+<BR>
+<button onclick="window.location.href='listedemandes.php'">Voir les demandes</button>
+
+<formulaire-menu/>
+</html> 
+
---
+++ repar/demande.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+  <script src="../boutik/article-list-item.js"></script>
+  <script src="../ux/menu.js"></script>
+  <style type="text/css">
+    body {
+      padding: 50px 10px;
+    }
+  </style>
+  <link href="../boutik/article-list.css" rel="stylesheet">
+</head>
+
+
+<form method="POST" action="../phpServer/mail.php">
+  <h2>Titre de la demande de réparation</h2>
+<div id="titre">
+	<input type="text" name="titre" class="form"  value="Je veux réparer un truc" onkeyup="showHint(this.value)"/>
+</div>
+<div id="descriptif">
+  <H3>Descriptif</H3>
+	<input type="text" name="descriptif" class="form"  value="Ca marche pas" onkeyup="showHint(this.value)"/>
+</div>
+
+	<input type="submit" value="envoyer la demande à : ">
+	<input type="text" style="hidden" name="dest" class="form"  value="baiorepar@framalistes.org" onkeyup="showHint(this.value)"/>
+</form>
+
+<br>
+<button type="button" onclick="reset()">bouton reset</button>
+
+</body>
+
+<script>
+
+function reset() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("titre").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "../static/titreReparation");
+  xhttp.send();
+
+}
+function testMail(dest) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("demo").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "../phpServer/mail.php");
+  xhttp.send();
+}
+</script>
+
+<formulaire-menu/>
+</html> 
+
