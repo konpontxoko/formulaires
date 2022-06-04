89deb2add6e27ffde2de38cdc5057a02a709e6dc
florian <florian@x230.pizzacoca.fr>
2022-06-04 08:37:56 +0200

    maj suivant modifs serveur

--- baiorepar/demande.php
+++ baiorepar/demande.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-<body>
 <head>
   <script src="/js/article-list-item.js"></script>
   <script src="/js/menu.js"></script>
@@ -10,51 +9,110 @@
     }
   </style>
   <link href="/css/article-list.css" rel="stylesheet">
+  <link href="/css/admin.css" rel="stylesheet">
 </head>
 
+<body>
 
-<form method="POST" action="/php/mail.php">
-  <h2>Titre de la demande de réparation</h2>
-<div id="titre">
-	<input type="text" name="titre" class="form"  value="Je veux réparer un truc" onkeyup="showHint(this.value)"/>
-</div>
-<div id="descriptif">
-  <H3>Descriptif</H3>
-	<input type="text" name="descriptif" class="form"  value="Ca marche pas" onkeyup="showHint(this.value)"/>
-</div>
+  <H2>Test 1 : Formulaire</H2>
+  <form method="POST" action="/baiorepar/mail.php">
+    <h2>Titre de la demande de réparation</h2>
+    <div id="titre">
+  	  <input type="text" name="titre" class="form"  value="Je veux faire une demande à" onkeyup="showHint(this.value)"/>
+  	  <input type="label" style="hidden" name="dest" class="form"  value="baiorepar@framalistes.org" onkeyup="showHint(this.value)"/>
+	  <BR>
+      <button type="button" onclick="reset()">bouton reset</button>
+    </div>
+    <div id="descriptif">
+      <H3>Descriptif</H3>
+  	  <input type="text" name="descriptif" class="form"  value="Ca marche pas" onkeyup="showHint(this.value)"/>
+    </div>
 
-	<input type="submit" value="envoyer la demande à : ">
-	<input type="text" style="hidden" name="dest" class="form"  value="baiorepar@framalistes.org" onkeyup="showHint(this.value)"/>
-</form>
+  	<input type="submit" value="envoyer la demande">
+  </form>
+  
+  
+  <hr>
+  <H2>Test 2 : PHPMailer</H2>
+ 
+  <button onclick="window.location.href='/baiorepar/phpmailer.php'">Demander une réparation</button>
+  <?php
+    // Création de la classe PHPMailer
+    use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+    
+    // Initialisation de PHPMailer
+    require '/php/PHPMailer/src/Exception.php';
+    require '/php/PHPMailer/src/PHPMailer.php';
+    require '/php/PHPMailer/src/SMTP.php';
+    
+    
+    // On crée un objet PHPMailer
+    $mail = new PHPMailer();
+    
+    // Configuration SMTP
+    $mail->isSMTP();
+    $mail->Host = 'smtp.mail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'baiorepar@mail.com'; 
+    $mail->Password = 'T02GqkYCpg82ixY' ;
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    
+    // Spécification des headers
+    $mail->setFrom('baiorepar@mail.com', 'Baiorepar');
+    $mail->addReplyTo('baiorepar@mail.com', 'Baiorepar');
+    $mail->addAddress('coucou@pizzacoca.fr', 'pizzacoca');
+    $mail->addCC('coucou@pizzacoca.fr', 'pizzacoca');
+    $mail->addBCC('contact@pizzacoca.fr', 'contact');
+    
+    // Sujet
+    $mail->Subject = 'Test Email via mail.com SMTP using PHPMailer';
+    
+    // Format du mail
+    $mail->isHTML(true);
+    
+    // Contenu du mail
+    $mailContent = "<h1>Send HTML Email using SMTP in PHP</h1>
+        <p>This is a test email I’m sending using SMTP mail server with PHPMailer.</p>";
+    $mail->Body = $mailContent;
 
-<br>
-<button type="button" onclick="reset()">bouton reset</button>
-
-</body>
-
-<script>
-
-function reset() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("titre").innerHTML =
-    this.responseText;
-  }
-  xhttp.open("GET", "/static/titreReparation");
-  xhttp.send();
-
-}
-function testMail(dest) {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("demo").innerHTML =
-    this.responseText;
-  }
-  xhttp.open("GET", "/php/mail.php");
-  xhttp.send();
-}
-</script>
-
-<formulaire-menu/>
+    if(!$mail->send()){
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
+	}
+	else{
+       echo 'Message has been sent'; 
+	 }
+	// Set mailer with this method
+	//$mailer->isSendmail();
+	$mailer->isQmail();
+	$mail->SMTPDebug = 2;
+    // And send mail as usual
+    $mailer->send();
+  ?>
+  		<formulaire-menu/>
+  		
+  <script>
+    function reset() {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        document.getElementById("titre").innerHTML =
+        this.responseText;
+      }
+      xhttp.open("GET", "/static/titreReparation");
+      xhttp.send();
+    
+    }
+    function testMail(dest) {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        document.getElementById("demo").innerHTML =
+        this.responseText;
+      }
+      xhttp.open("GET", "/phpServer/mail.php");
+      xhttp.send();
+    }
+  </script>
 </html> 
 
--- index.php
+++ index.php
@@ -7,6 +7,48 @@
     body {
       padding: 50px 10px;
     }
+ <style>
+    .Admin {
+      background: #ddd;
+      height: 100%;
+      display: grid;
+      grid-template-rows: 1fr 1fr 1fr;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-areas:
+      ". . ."
+      "new pending archives"
+      ". . database";
+    }
+    .Admin > * {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      color: black;
+      text-transform: uppercase;
+    }
+    .Admin > *:hover {
+      font-weight: bold;
+    }
+    .Admin-new {
+      grid-area: new;
+      background: #eee;
+    }
+    .Admin-pending {
+      grid-area: pending;
+      background: #ccc;
+    }
+    .Admin-archives {
+      grid-area: archives;
+      background: #eee;
+    }
+    .Admin-database {
+      grid-area: database;
+      background: #ccc;
+    }
+  </style>
+
+
   </style>
   <link href="/css/article-list.css" rel="stylesheet">
   <link href="/css/admin.css" rel="stylesheet">
@@ -18,6 +60,7 @@
   <body class="Admin">
     <a class="Admin-new" href="/baiorepar/baiorepar.php">BAIOREPAR</a>
     <a class="Admin-pending" href="/boutik/boutik.php">BOUTIK</a>
+    <a class="Admin-archives" href="/boutik/boutik.php">BATTERIES</a>
   </body>
 
 
