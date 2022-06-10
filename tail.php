af9212097f70404f09f39e8c4ca8608aec1d5cd7
baiorepar <baiorepar@site-baiorepar.baiorepar.fr>
2022-06-10 20:47:49 +0200

    modif footer & css

--- tail.php
+++ tail.php
@@ -2,11 +2,11 @@
 <br/>
 <br/>
 <HR>
-      <footer class="footer pull-down">
-      	<p class="pull-right">Designed  <a
-          href="https://youtu.be/77hwpuVHGps?t=435">without bootstrap</a>.</p>
-      	<p class="pull-left">Mentions <a
+      <footer class="footer">
+      	<p class="foot-left">Mentions <a
 	  href="/rgpd.php">RGPD</a>.</p>
+      	<p class="foot-right">Designed  <a
+          href="https://youtu.be/77hwpuVHGps?t=435">without bootstrap</a>.</p>
       </footer>
 
     </div><!-- container -->
