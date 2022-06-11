2176fa7f3406247effa06f896617afc70a7ca814
baiorepar <baiorepar@site-baiorepar.baiorepar.fr>
2022-06-11 12:38:41 +0200

    maj scripts sql

--- sql/init.sh
+++ sql/init.sh
@@ -1,4 +1,8 @@
-echo "creation table"
-mysql -u site_db_admin -p <  01_creation_baiorepar.sql
-echo "injection données"
-mysql -u site_db_admin -p <  04_insertion_baiorepar.sql
+echo "quelle base de donnée doit etre mise à jour ?"
+echo -e "main \n flo \n alex \n yacou "
+read dbUser
+
+echo "ecrasement tables db_$dbUser"
+mysql -u admin_db_$dbUser -p <  01_creation_baiorepar.sql
+echo "injection données dans db_$dbUser"
+mysql -u admin_db_$dbUser -p <  04_insertion_baiorepar.sql
